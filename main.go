09896f5e0c26095805f194035e752eae13d84bee
Balamurugan Venkadachalam <engan.balamurugan@gmail.com>
2021-09-18 19:36:41 +1200

    echo rest api Get

--- main.go
+++ main.go
@@ -2,15 +2,41 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"net/http"
+	"os"
+	"strconv"
 )
 
 func main() {
-	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(200, "homepage")
-	})
-	err := e.Start(":8081")
-	if err != nil {
-		return
+	port := os.Getenv("MY_APP_PORT")
+	if port == "" {
+		port = "8081"
 	}
+	e := echo.New()
+	products := []map[int]string{{1: "product1"}}
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "homepage, hello world !!!")
+	})
+	e.GET("/product/:id", func(context echo.Context) error {
+		var product map[int]string
+		for _, p := range products {
+			for key := range p {
+				pID, err := strconv.Atoi(context.Param("id"))
+				if err != nil {
+					return err
+				}
+				if pID == key {
+					product = p
+				}
+			}
+		}
+		if product == nil {
+			return context.JSON(http.StatusNotFound, "product not found")
+		}
+
+		return context.JSON(http.StatusOK, product)
+	})
+	e.Logger.Print("Listening on port 8081")
+	e.Logger.Fatal(e.Start(":8081"))
 }
